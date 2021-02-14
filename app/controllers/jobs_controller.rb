f623577e5482622ccbefa0fc041f2142d538df9d
Hagen <hagen.sopher@tamu.edu>
2021-02-14 17:42:27 -0600

    most recent push for jobs, note line 10 in the controller not working

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,8 +2,12 @@ class JobsController < ApplicationController
   def index
     @job = Job.all
   end
-
+  def new
+    @job = Job.new
+  end
   def create
+    #this will need some sort of authentication to make it admin only, right now I am just creating the page
+    @job = Job.create(params.require(:))
   end
 
   def show
@@ -13,5 +17,9 @@ class JobsController < ApplicationController
   end
 
   def delete
+  end
+
+  def job_params
+    params.require(:job).permit(:company, :description)
   end
 end
