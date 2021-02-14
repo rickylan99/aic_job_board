edcf7e00a14d07af58efca8037e0ba5fb3b4d774
LordDaedhelor <57498866+LordDaedhelor@users.noreply.github.com>
2021-02-14 15:04:06 -0600

    User Done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,52 @@
 class UsersController < ApplicationController
+  
   def index
-  end
-
-  def create
+    @users = User.order('title ASC')
+    @to_print = @@message
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to(users_path)
+    else
+      render('new')
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to(user_path(@user))
+    else
+      render('edit')
+    end
   end
 
   def delete
+    @user = User.find(params[:id])
   end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to(users_path)
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:email,:password,:roleid)
+    end
 end
