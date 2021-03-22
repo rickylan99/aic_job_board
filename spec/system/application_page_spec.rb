4da4668c3ad0858dfda16ddf1f5a5c17eae2acf3
hagen.sopher <hagen.sopher@tamu.edu>
2021-03-22 17:59:20 -0500

    new application rspec test

---
+++ spec/system/application_page_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+RSpec.describe 'Job Page'  do
+  before(:each) do
+    #creates a user and auto logins in
+    user = User.create(email: "test@gmail.com", password: "test")
+    visit root_path
+      within 'div#loginBox' do
+        fill_in 'Email', with: 'test@gmail.com'
+        fill_in 'Password', with: 'test'
+        click_on 'Log In'
+      end
+  end
+  
+  describe 'Applying to new User' do
+    it 'Creates a user application' do     
+        click_on "About Access"
+        expect(page).to have_text("What is AIC ACCESS?")
+        click_on "About_Button"
+        expect(page).to have_text("Application Form")
+        fill_in 'First name', with: 'test@gmail.com'
+        fill_in 'Last name', with: 'test'
+        fill_in 'Email', with: 'test@gmail.com'
+        fill_in 'Phone number', with: 'test'
+        #select 'Freshmen', from 'access_submission_classification'
+        fill_in 'access_submission_q1', with: 'I love this club so much'
+        fill_in 'access_submission_q2', with: 'I love AIC access'
+        fill_in 'access_submission_q3', with: 'I love AIC club so much'
+        fill_in 'access_submission_q4', with: 'I club so much'
+        click_on 'Apply'
+        expect(page).to have_text("Application Submitted Sucessfully!")
+    end
+  end
+  
+end
