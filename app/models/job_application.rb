ce5aba17de813cfc1a1d7e02b497aad379842352
ricky-99 <ricky.99@tamu.edu>
2021-03-23 00:38:25 -0500

    Added styling for all job, firms, and job application features

--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -1,4 +1,5 @@
 class JobApplication < ApplicationRecord
+    belongs_to :user
     belongs_to :job
     has_and_belongs_to_many :documents
     has_many :job_application_answers, dependent: :destroy 
