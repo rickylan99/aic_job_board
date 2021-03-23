939f9f699a0dc1e1d1c63dec25afe64a1c7f1afe
ricky-99 <ricky.99@tamu.edu>
2021-03-23 01:40:10 -0500

    Fix seed and change button color

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,6 @@ Role.create(roletype: "Student")
 user = User.create(email: "test@gmail.com", first_name: "First", last_name: "Last", password: "test", role_id: Role.find_by_roletype("Student").id)
 admin = User.create(email: "admin@gmail.com", first_name: "Admin", last_name: "Last", password: "admin", role_id: Role.find_by_roletype("Admin").id)
 
-firm = Firm.create(name: "JP Morgan", industry: "Banking", location: "New York City", description: "JPMorgan Chase & Co. is an American multinational investment bank and financial services holding company headquartered in New York City. JPMorgan Chase is ranked by S&P Global as the largest bank in the United States and the seventh largest bank in the world by total assets, with total assets of US$3.213 trillion.", website: "https://www.jpmorganchase.com/", linkedin: "https://www.linkedin.com/company/jpmorgan/", user_id: user.id)
+firm = Firm.create(name: "JP Morgan", industry: "Banking", location: "New York City", description: "JPMorgan Chase & Co. is an American multinational investment bank and financial services holding company headquartered in New York City. JPMorgan Chase is ranked by S&P Global as the largest bank in the United States and the seventh largest bank in the world by total assets, with total assets of US$3.213 trillion.", website: "https://www.jpmorganchase.com/", linkedin: "https://www.linkedin.com/company/jpmorgan/")
 
 
