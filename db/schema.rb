992ff20422e0bb6e6b7c415347b8c3f67434a727
ricky-99 <ricky.99@tamu.edu>
2021-04-04 19:31:33 -0500

    Add firm contact details

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_183806) do
+ActiveRecord::Schema.define(version: 2021_04_04_235016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,9 @@ ActiveRecord::Schema.define(version: 2021_03_22_183806) do
     t.string "linkedin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "contact_name"
+    t.string "contact_email"
+    t.string "contact_linkedin"
   end
 
   create_table "job_application_answers", force: :cascade do |t|
