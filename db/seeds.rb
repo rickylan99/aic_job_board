3df4f43e0f4e48bfe1c4f84a2a837cf75b7b01a8
zaalb99 <zaalb@hotmail.com>
2021-04-16 18:09:16 -0500

    Added new rake and adjusted seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,10 +17,6 @@ if Role.count == 0
 end
 
 if User.count == 0
-    user = User.create(email: "test@gmail.com", first_name: "First", last_name: "Last", password: "test", role_id: Role.find_by_roletype("Student").id)
-    user = User.create(email: "test1@gmail.com", first_name: "First1", last_name: "Last1", password: "test", role_id: Role.find_by_roletype("Student").id)
-    user = User.create(email: "test2@gmail.com", first_name: "First2", last_name: "Last2", password: "test", role_id: Role.find_by_roletype("Student").id)
-    user = User.create(email: "test3@gmail.com", first_name: "First3", last_name: "Last3", password: "test", role_id: Role.find_by_roletype("Student").id)
     admin = User.create(email: "admin@gmail.com", first_name: "AdminFirst", last_name: "AdminLast", password: "admin", role_id: Role.find_by_roletype("Head Admin").id)
 end
 
---
+++ lib/tasks/generate_users.rake
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+desc 'This task generates 100 users'
+task generate_users: :environment do
+  puts 'Generating users...'
+
+  public_id = "shfgjkg7sc0bkyvbc0ca"
+
+  for i in 1..100 do
+    puts i
+    User.create(email: "test_user#{i}@gmail.com", password: "test", first_name: "User#{i}", last_name: "Last",
+                role_id: Role.find_by(roletype: 'Student').id).documents.new(public_id: public_id, file_name: "Z.pdf", 
+                documenttype: 'resume').save
+  end
+
+  puts 'Done.'
+end
